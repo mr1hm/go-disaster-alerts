10c1a1b3e16ebdf9278bbec73b8c8fab1bcde7bf
Kevin Ihm <kevinihm@gmail.com>
2026-02-16 20:24:30 -0800

    updated slog to use local timestamps

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -23,6 +23,12 @@ func Setup(level string) {
 
 	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: logLevel,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				a.Value = slog.StringValue(a.Value.Time().Local().Format("2006-01-02T15:04:05.000-07:00"))
+			}
+			return a
+		},
 	})
 
 	slog.SetDefault(slog.New(handler))
